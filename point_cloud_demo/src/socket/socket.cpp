b31ad2b043e8f63250713b3402e391f764bd8f73
yingjie_wang <2778809626@qq.com>
2024-01-05 17:38:18 +0800

    异步socket client

--- point_cloud_demo/src/socket/socket.cpp
+++ point_cloud_demo/src/socket/socket.cpp
@@ -4,6 +4,7 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <thread>
+#include <chrono>
 
 
 
@@ -95,59 +96,62 @@ bool Socket::tcpClientSync(std::string ip, int port)
 }
 
 
-bool reconnect = false;
+bool g_isRunning = true;
 
-void receiveData(SOCKET clientSocket)
+void DataReceiver(SOCKET clientSocket)
 {
-    while (true)
+    char buffer[1024];
+    while (g_isRunning)
     {
-        char buffer[1024];
         memset(buffer, 0, sizeof(buffer));
-
-        int bytesRead = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
-
-        if (bytesRead == SOCKET_ERROR)
+        int is_rec = recv(clientSocket, buffer, sizeof(buffer), 0);
+        if (is_rec == 0)
         {
-            std::cout << "Failed to receive data: " << WSAGetLastError() << std::endl;
-            reconnect = true;
-            return;
+            std::cout << "Server disconnection and reconnection" << std::endl;
+            break;
         }
-        if (bytesRead == 0)
+        else if (is_rec < 0)
         {
-            std::cout << "Server disconnected." << std::endl;
-            reconnect = false;
-            return;
+            std::cout << "Failed to receive data" << std::endl;
+            break;
         }
-
-        std::string receivedData(buffer, bytesRead);
-        std::cout << "Received data: " << receivedData << std::endl;
+        else
+        {
+            std::cout << "Received data from server: " << buffer << std::endl;
+            // 处理接收到的数据
+        }
     }
+    closesocket(clientSocket);
+    return;
 }
+
 
 bool Socket::tcpClientAsyn(std::string ip, int port)
 {
     WSADATA wsaData;
     if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
     {
-        std::cout << "Failed to initialize winsock." << std::endl;
-        return false;
-    }
-
-    SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (clientSocket == INVALID_SOCKET)
-    {
-        std::cout << "Failed to create socket: " << WSAGetLastError() << std::endl;
-        WSACleanup();
+        std::cout << "Failed to initialize winsock" << std::endl;
         return false;
     }
     while (true)
     {
+        // 创建套接字
+        SOCKET clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+        if (clientSocket == INVALID_SOCKET)
+        {
+            std::cout << "Failed to create socket" << std::endl;
+            WSACleanup();
+            return false;
+        }
+
+        // 设置服务器信息
         sockaddr_in serverAddress;
         serverAddress.sin_family = AF_INET;
         serverAddress.sin_port = htons(port);
-        if (inet_pton(AF_INET, ip.c_str(), &(serverAddress.sin_addr)) != 1)
+        if (inet_pton(AF_INET, ip.c_str(), &(serverAddress.sin_addr)) <= 0)
         {
-            std::cout << "Invalid server IP address." << std::endl;
+            std::cout << "Invalid address/Address not supported" << std::endl;
             closesocket(clientSocket);
             WSACleanup();
             return false;
@@ -155,41 +159,45 @@ bool Socket::tcpClientAsyn(std::string ip, int port)
 
         while (true)
         {
+            // 连接到服务器
             int is_con = connect(clientSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
             if (is_con == SOCKET_ERROR)
             {
-                std::cout << "Failed to connect to the server: " << WSAGetLastError() << std::endl;
-                std::this_thread::sleep_for(std::chrono::seconds(3)); // 等待3秒再重新连接
-                continue;
+                std::cout << "Connection failed" << std::endl;
+                std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // 等待1秒后重新连接
+                break;
             }
-
-            reconnect = false;
-            std::thread receiveThread(receiveData, clientSocket);
-
-            while (true)
+            else
             {
-                if (reconnect)
+                std::cout << "Connected to server" << std::endl;
+                std::thread receiverThread(DataReceiver, clientSocket);
+                receiverThread.detach();
+
+                int n = 0;
+                while (true)
                 {
-                    receiveThread.join();
-                    closesocket(clientSocket);
-                    break;
+                    std::cout << n++ << std::endl;
+                    std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // 等待1秒后重新连接
+
+                    // 检查连接状态
+                    int error = 0;
+                    int errorLen = sizeof(error);
+                    int ret = getsockopt(clientSocket, SOL_SOCKET, SO_ERROR, (char*)&error, &errorLen);
+                    if (ret == SOCKET_ERROR || error != 0)
+                    {
+                        break;
+                    }
                 }
-
-                // 执行其他工作
-                // ...
-                std::cout << "a";
-
-                std::this_thread::sleep_for(std::chrono::milliseconds(100)); // 等待一段时间再继续接收数据
             }
         }
 
         closesocket(clientSocket);
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000)); // 等待1秒后重新连接
     }
 
     WSACleanup();
     return true;
 }
-
 
 
 
